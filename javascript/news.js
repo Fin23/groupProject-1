5a0065ec311afcf31288da22af9d82b13ee26a10
Ross Jameson <Rossjameson1623@gmail.com>
2019-07-20 11:25:51 -0500

    fire base functions with news nav bar button clicks on catagories

--- javascript/news.js
+++ javascript/news.js
@@ -1,1 +1,40 @@
+
+$(document).ready(function(){
+    
+
+
+
 var newsQuery = "https://api-beta.civicfeed.com/news/search?results=10" + type + "";
+
+$.ajax({
+    url:queryURL,
+    method:"GET"
+})
+
+
+
+//end of document.ready fn.
+});
+//nav bar functions for buttons
+
+
+// function News(){
+    
+
+// }
+// function Politics(){
+
+// }
+// function Tech(){
+
+// }
+// function Style(){
+
+// }
+// function Sports(){
+
+// }
+// function Food(){
+
+// }
+//end of nav bar button functions
