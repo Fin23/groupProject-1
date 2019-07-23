82bb289cd5100e1a36c399650e656ac133af39e6
Ross <rossjameson1623@gmail.com>
2019-07-23 16:53:16 -0500

    adding api function

--- javascript/news.js
+++ javascript/news.js
@@ -1,18 +1,62 @@
 
-$(document).on("click", function(){
-    $("")
+$(document).ready(function(){
+$("#searchbtn").on("click", function(e){
+e.preventDefault();
+
+let query = $("#searchquery").val();
+let url = "https://newsapi.org/v2/everything?q="+query+"domains=wsj.com&apiKey=9a1aa1cc59c9499790a0a0882960d673";
+console.log(url);
+
+if(query !==""){
     
+ $.ajax({
 
 
-
-var newsQuery = "https://api-beta.civicfeed.com/news/search?results=10" ;
-
-$.ajax({
-    url:queryURL,
-    method:"GET"
-})
+     url:url,
+     method:"GET",
+     dataType:"json",
 
 
+     beforSend: function(){
+            $("#loader").show();
+     },
+complete: function(){
+    $("#loader").hide();
+},
+success: function(news){
+console.log(news);
+},
+error: function(){
+    console.log("error");
+}
+ })
 
-//end of document.ready fn.
+}else{
+    console.log("please enter something")
+}
 });
+
+});
+
+
+
+
+ 
+    
+//google news api
+//apikey=9a1aa1cc59c9499790a0a0882960d673
+
+//civicFeed api
+//"https://api-beta.civicfeed.com/news/search?q="+query+"&apiKey=IUc327VfgV8wXOBxEmFge7BZplpHPmhF7sFDC5rV";
+
+ 
+
+// $.ajax({
+//     url:queryURL,
+//     method:"GET"
+// })
+
+
+
+// //end of document.ready fn.
+// });
