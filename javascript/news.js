a333a24c79d7f6ae427a0765dee501e63bd504a0
Ross Jameson <Rossjameson1623@gmail.com>
2019-07-25 16:29:19 -0500

    updated news javascript, html, css, looks better functions more, home and business btns send to page, but not done

--- javascript/news.js
+++ javascript/news.js
@@ -4,8 +4,13 @@ $(document).ready(function(){
     e.preventDefault();
     
     let query = $("#searchquery").val();
-    let url = "https://newsapi.org/v2/top-headlines?q="+query+"&country=us&category=business&apiKey=9a1aa1cc59c9499790a0a0882960d673";
+    //civicFeed api
+    //let url = "https://api-beta.civicfeed.com/legislation/metadata accept: application/json X-API-KEY: IUc327VfgV8wXOBxEmFge7BZplpHPmhF7sFDC5rV";
+
+    let url = "https://newsapi.org/v2/top-headlines?q="+query+"&apiKey=9a1aa1cc59c9499790a0a0882960d673";
     console.log(url);
+
+  
     
     if(query !==""){
         
@@ -13,7 +18,9 @@ $(document).ready(function(){
          url:url,
          method:"GET",
          dataType:"json",
-         beforSend: function(){
+         
+
+         beforeSend: function(){
                 $("#loader").show();
          },
     complete: function(){
@@ -21,41 +28,45 @@ $(document).ready(function(){
     },
     success: function(news){
     console.log(news);
-     let output = "";
-    let latestNews = news.articles;
+        let output = "";
+        let latestNews = news.articles;
     
     for(var i in latestNews){
         output += `
-        <div class="five2">
-        <h4>${latestNews[i].title}</h4>
-        
-        <p>${latestNews[i].description}</p>
-        <p>${latestNews[i].content}</p>
-        <p>Publish on: ${latestNews[i].publishedAt}</p>
-        <a href="${latestNews[i].url}" class ="btn"> Read some news</a>
+       <div class="border1" style= 'font-size: 20px;'>
+      <p> <img src="${latestNews[i].urlToImage}" <div style = 'height:120px; width:170px;'></img>
+        ${latestNews[i].title}
+       
+        ${latestNews[i].description}
+        <br>
+       Source: ${latestNews[i].source.name}
+        <br>
+        Publish on: ${latestNews[i].publishedAt}
+        <br>
+        <a href="${latestNews[i].url}" class ="btn"> Read some news</a></p>
        </div>
         `;
     }
     if(output !== ""){
         $("#recent_news").html(output);
         M.toast({
-            html: "there you go, nice reading",
-            classes: 'green'
-        })
+            html: " you did it, good job....",
+            classes: 'blue'
+        });
 
     }else{
         let NewsNotFound = "This news isn't available.";
-        $("#recent_news").html("<div style = ' font-size:40px; text-align:center;'>"+ NewsNotFound+"</div>")
+        $("#recent_news").html("<div style = ' font-size:40px; text-align:center;'>"+ NewsNotFound+"</div>");
         
     }
     },
     error: function(){
         console.log("error");
     }
-     })
+     });
     
     }else{
-        console.log("please enter something")
+        console.log("please enter something");
     }
     });
     
@@ -66,11 +77,11 @@ $(document).ready(function(){
     
      
         
-    //google news api
-    //apikey=9a1aa1cc59c9499790a0a0882960d673
     
-    //civicFeed api
-    //let url = "https://api-beta.civicfeed.com/codify/search?&apiKey=IUc327VfgV8wXOBxEmFge7BZplpHPmhF7sFDC5rV";
+    
+    
     
      
-//<img src="${latestNews[i].urlToImage}"></img>    col 16 m6 s12
+//<img src="${latestNews[i].urlToImage}"></img>    and <p>${latestNews[i].content}</p>   <div class="six">
+//col 16 m6 s12  
+//api notes currently using news api 
